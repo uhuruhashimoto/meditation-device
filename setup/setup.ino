44bfd71a297c03bf3660a447ee74edd321737f07
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2024-01-07 18:21:34 -0500

    file storage

--- setup/setup.ino
+++ setup/setup.ino
@@ -6,6 +6,8 @@
 #include <SD.h>
 const int chipSelect = SDCARD_SS_PIN;
 void setup() {
+  pinMode(10, OUTPUT); // change this to 53 if you are using arduino mega
+  digitalWrite(10, HIGH); // Add this line
   // put your setup code here, to run once:
   Serial.begin(9600);
   while (!Serial) {
@@ -18,12 +20,18 @@ void setup() {
     while (1);
   }
   Serial.println("Card initialized.");
+
+  // initialize digital pin LED_BUILTIN as an output.
+  //pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   while (1) {
-    ;
+//      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//      delay(1000);                       // wait for a second
+//      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//      delay(1000); 
   }
 
 }
